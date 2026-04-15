dbb67da0fc423433d156e3b5ca298700c3aa9f11
Kalel Fernandes <kalelfernandes.senai@gmail.com>
2026-04-15 15:42:02 -0300

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+function addTask() {
+    const name = document.getElementById('name-task');
+    const time = document.getElementById('hours');
+    const costHour = document.getElementById('hourly-rate');
+
+    const validateInput = () => inputElement.value.trim().length > 0;
+}
